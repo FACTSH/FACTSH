6daeaa716773964a3987665f936d2696e8d7c2a0
Ashly Ann Jo <ashlyannjo.phd2112@iiitkottayam.ac.in>
2024-07-20 12:59:39 +0530

    updated

--- src/Content/LatestNews.js
+++ src/Content/LatestNews.js
@@ -7,7 +7,15 @@ const News = [
        {
         "news":"Susmera Joseph, Ashly Ann Jo and Ebin Deni Raj's paper on \"Improving Time Series Forecasting Accuracy with Transformers: A Comprehensive Analysis with Explainability \" is accepted for the Third International Conference on Electrical, Electronics, Information and Communication Technologies (ICEEICT 2024).",
         "description":"Accepted"
-       }
+       },
+       {
+            "news":"Ranjith R, Ashly Ann Jo and Ebin Deni Raj's paper on \"Bipol-Driven Bias Analysis in Transformer Models: A Quantitative, Global, and Local Interpretability Approach for Textual Data \" is accepted for the IEEE World Conference on Applied Intelligence and Computing (AIC 2024).",
+            "description":"Accepted"
+      },
+      {
+            "news":"Anish Reddy Konyala, Ashly Ann Jo and Ebin Deni Raj's paper on \"Integrating Explainable AI with Machine Learning for Accurate Liver Disease Diagnosis\" is accepted for the 2nd International Conference on Artificial Intelligence: Theory and Applications(AITA 2024).",
+             "description":"Accepted"
+      }
       
   ];
   export default News
