02bcb2bf40b9296ef755b1f2d3ab054d33b09ee4
Ashly Ann Jo <ashlyannjo.phd2112@iiitkottayam.ac.in>
2024-07-20 10:39:27 +0530

    updated repo

--- src/Content/LatestNews.js
+++ src/Content/LatestNews.js
@@ -1,17 +1,11 @@
 const News = [
-       {
-        "news":"Mahesh Sasikumar, and Ebin Deni Raj. \"A short survey on Interpretable techniques with Time series data.\" In 2024 IEEE International Students' Conference on Electrical, Electronics and Computer Science (SCEECS), pp. 1-10. IEEE, 2024.",
-        "link":"",
-        "description":"Published"
-       },
-       {
-        "news":"Subodh Uniyal, Maitri D. Savla, Tushaar, Ashly Ann Jo, and  Ebin Deni Raj. “Transparency in Legal AI: Pioneering Explainable NLP for  Enhanced Judgement Prediction” accepted for publication in the International Conference on Data Science & Exploration in Artificial Intelligence(CODE-AI), 2024.",
+              {
+        "news":"Subodh Uniyal, Maitri D. Savla, Tushaar, Ashly Ann Jo, and  Ebin Deni Raj Received Best Paper Award for the paper “Transparency in Legal AI: Pioneering Explainable NLP for  Enhanced Judgement Prediction”  in the International Conference on Data Science & Exploration in Artificial Intelligence(CODE-AI), 2024.",
         "link":"",
         "description":"Accepted"
        },
        {
-        "news":"Ashly Ann Jo and Ebin Deni Raj's paper on \"Ethical Dimensions in Data-Driven Business\" is accepted for the 15th International IEEE Conference On Computing, Communication And Networking Technologies 2024.",
-        "link":"",
+        "news":"Susmera Joseph, Ashly Ann Jo and Ebin Deni Raj's paper on \"Improving Time Series Forecasting Accuracy with Transformers: A Comprehensive Analysis with Explainability \" is accepted for the Third International Conference on Electrical, Electronics, Information and Communication Technologies (ICEEICT 2024).",
         "description":"Accepted"
        }
       
