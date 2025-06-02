7512811e00d2c1ccd90336e9967cbb8bbdfecef4
AlMIGHTY-HARDIK <somkuwar21bcy46@iiitkottayam.ac.in>
2025-06-02 23:07:03 +0530

    added youtube news

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -1,38 +1,33 @@
-// App.js
+// src/components/Gallery/Gallery.js
 import React from 'react';
-import galleryimage from '../../Content/galleryimages';
 import Masonry from 'react-masonry-css';
-import ImageCard from '../../components/ImageCard/Imagecard';
 import Heading from '../../components/Heading/Heading';
-import './gallery.css'
+import ImageCard from '../../components/ImageCard/Imagecard';
+import galleryimage from '../../Content/galleryimages';
+import './gallery.css';
+
 const Gallery = () => {
   const breakpointColumnsObj = {
-    default: 3,
-    1100: 2,
-    800: 1,
-    500: 1
+    default: 3,  // 3 columns on wide screens
+    1100: 2,     // 2 columns ≥ 1100px
+    800: 1,      // 1 column ≥ 800px
+    500: 1       // 1 column ≥ 500px (basically mobile)
   };
 
   return (
     <div className="gallery">
       <Heading content="Gallery" />
       <Masonry
-      breakpointCols={breakpointColumnsObj}
-      className="masonry-grid"
-      columnClassName="masonry-grid_column"
-    >
-      {galleryimage.map((image, index) => (
-        <ImageCard key={index} image={image} />
-      ))}
-    </Masonry>
+        breakpointCols={breakpointColumnsObj}
+        className="masonry-grid"
+        columnClassName="masonry-grid_column"
+      >
+        {galleryimage.map((image, index) => (
+          <ImageCard key={index} image={image} />
+        ))}
+      </Masonry>
     </div>
   );
 };
 
 export default Gallery;
-
-
-
-
-
-
